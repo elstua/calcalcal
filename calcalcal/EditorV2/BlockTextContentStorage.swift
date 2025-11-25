6378498e1f3ca72bc5ec9f59ddb04e794ca2b61c
elstua <thestua@gmail.com>
2025-11-25 23:39:20 +0000

    UI: Fix padding for the images and bug with larger paddings after that

--- calcalcal/EditorV2/BlockTextContentStorage.swift
+++
@@ -1,5 +0,0 @@
-// This file previously contained a custom `NSTextContentStorage` subclass.
-// The architecture has been simplified so that TextKit 2's default
-// content storage is used, and block metadata is managed by
-// `BlockDocumentController` instead.
-
