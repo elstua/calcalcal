8950f724a963e7804d7c627348533d3df5f6bd60
elstua <thestua@gmail.com>
2026-02-08 23:13:55 +0000

    UI: changes in streak, updates for gallery, a lot of tweaks for editor design

--- calcalcal/Utilities/VariableBlur.swift
+++ calcalcal/Utilities/VariableBlur.swift
@@ -1,118 +1,103 @@
-// VariableBlur – progressive/variable blur (gradual blur by position).
-// Adapted from https://github.com/nikstar/VariableBlur
-// Used for header progressive blur per https://designcode.io/swiftui-handbook-progressive-blur
 
 import SwiftUI
 import UIKit
 import CoreImage.CIFilterBuiltins
 import QuartzCore
 
-enum VariableBlurDirection {
+
+public enum VariableBlurDirection {
     case blurredTopClearBottom
     case blurredBottomClearTop
 }
 
-struct VariableBlurView: UIViewRepresentable {
-    var maxBlurRadius: CGFloat = 20
-    var direction: VariableBlurDirection = .blurredTopClearBottom
-    var startOffset: CGFloat = 0
 
-    init(maxBlurRadius: CGFloat = 20, direction: VariableBlurDirection = .blurredTopClearBottom, startOffset: CGFloat = 0) {
+public struct VariableBlurView: UIViewRepresentable {
+    
+    public var maxBlurRadius: CGFloat = 20
+    
+    public var direction: VariableBlurDirection = .blurredTopClearBottom
+    
+    /// By default, variable blur starts from 0 blur radius and linearly increases to `maxBlurRadius`. Setting `startOffset` to a small negative coefficient (e.g. -0.1) will start blur from larger radius value which might look better in some cases.
+    public var startOffset: CGFloat = 0
+    
+    public init(maxBlurRadius: CGFloat = 20, direction: VariableBlurDirection = .blurredTopClearBottom, startOffset: CGFloat = 0) {
         self.maxBlurRadius = maxBlurRadius
         self.direction = direction
         self.startOffset = startOffset
     }
-
-    func makeUIView(context: Context) -> VariableBlurUIView {
+    
+    public func makeUIView(context: Context) -> VariableBlurUIView {
         VariableBlurUIView(maxBlurRadius: maxBlurRadius, direction: direction, startOffset: startOffset)
     }
 
-    func updateUIView(_ uiView: VariableBlurUIView, context: Context) {
-        uiView.updateBlur(maxBlurRadius: maxBlurRadius)
+    public func updateUIView(_ uiView: VariableBlurUIView, context: Context) {
     }
 }
 
-/// Variable blur: blur strength follows a gradient (e.g. strong at top, clear at bottom).
-/// Credit: https://github.com/jtrivedi/VariableBlurView via https://github.com/nikstar/VariableBlur
-final class VariableBlurUIView: UIVisualEffectView {
 
-    private var variableBlurFilter: NSObject?
-    private var backdropLayer: CALayer?
-    private let direction: VariableBlurDirection
-    private let startOffset: CGFloat
+/// credit https://github.com/jtrivedi/VariableBlurView
+open class VariableBlurUIView: UIVisualEffectView {
 
-    init(maxBlurRadius: CGFloat = 20, direction: VariableBlurDirection = .blurredTopClearBottom, startOffset: CGFloat = 0) {
-        self.direction = direction
-        self.startOffset = startOffset
+    public init(maxBlurRadius: CGFloat = 20, direction: VariableBlurDirection = .blurredTopClearBottom, startOffset: CGFloat = 0) {
         super.init(effect: UIBlurEffect(style: .regular))
 
         let clsName = String("retliFAC".reversed())
-        guard let Cls = NSClassFromString(clsName) as? NSObject.Type else {
+        guard let Cls = NSClassFromString(clsName)! as? NSObject.Type else {
+            print("[VariableBlur] Error: Can't find filter class")
             return
         }
         let selName = String(":epyThtiWretlif".reversed())
-        guard let variableBlur = Cls.perform(NSSelectorFromString(selName), with: "variableBlur")?.takeUnretainedValue() as? NSObject else {
+        guard let variableBlur = Cls.self.perform(NSSelectorFromString(selName), with: "variableBlur").takeUnretainedValue() as? NSObject else {
+            print("[VariableBlur] Error: Can't create variableBlur filter")
             return
         }
-        variableBlurFilter = variableBlur
 
+        // The blur radius at each pixel depends on the alpha value of the corresponding pixel in the gradient mask.
+        // An alpha of 1 results in the max blur radius, while an alpha of 0 is completely unblurred.
         let gradientImage = makeGradientImage(startOffset: startOffset, direction: direction)
+
         variableBlur.setValue(maxBlurRadius, forKey: "inputRadius")
         variableBlur.setValue(gradientImage, forKey: "inputMaskImage")
         variableBlur.setValue(true, forKey: "inputNormalizeEdges")
 
-        if let layer = subviews.first?.layer {
-            backdropLayer = layer
-            layer.filters = [variableBlur]
-        }
+        // We use a `UIVisualEffectView` here purely to get access to its `CABackdropLayer`,
+        // which is able to apply various, real-time CAFilters onto the views underneath.
+        let backdropLayer = subviews.first?.layer
+
+        // Replace the standard filters (i.e. `gaussianBlur`, `colorSaturate`, etc.) with only the variableBlur.
+        backdropLayer?.filters = [variableBlur]
+        
+        // Get rid of the visual effect view's dimming/tint view, so we don't see a hard line.
         for subview in subviews.dropFirst() {
             subview.alpha = 0
         }
     }
 
-    func updateBlur(maxBlurRadius: CGFloat) {
-        variableBlurFilter?.setValue(maxBlurRadius, forKey: "inputRadius")
-    }
-
-    required init?(coder: NSCoder) {
+    required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    override func didMoveToWindow() {
-        super.didMoveToWindow()
+    
+    open override func didMoveToWindow() {
+        // fixes visible pixelization at unblurred edge (https://github.com/nikstar/VariableBlur/issues/1)
         guard let window, let backdropLayer = subviews.first?.layer else { return }
         backdropLayer.setValue(window.traitCollection.displayScale, forKey: "scale")
     }
-
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {}
-
-    private func makeGradientImage(width: CGFloat = 100, height: CGFloat = 100, startOffset: CGFloat, direction: VariableBlurDirection) -> CGImage {
-        let ciGradientFilter = CIFilter.linearGradient()
+    
+    open override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        // `super.traitCollectionDidChange(previousTraitCollection)` crashes the app
+    }
+    
+    private func makeGradientImage(width: CGFloat = 100, height: CGFloat = 100, startOffset: CGFloat, direction: VariableBlurDirection) -> CGImage { // much lower resolution might be acceptable
+        let ciGradientFilter =  CIFilter.linearGradient()
+//        let ciGradientFilter =  CIFilter.smoothLinearGradient()
         ciGradientFilter.color0 = CIColor.black
         ciGradientFilter.color1 = CIColor.clear
         ciGradientFilter.point0 = CGPoint(x: 0, y: height)
-        ciGradientFilter.point1 = CGPoint(x: 0, y: startOffset * height)
+        ciGradientFilter.point1 = CGPoint(x: 0, y: startOffset * height) // small negative value looks better with vertical lines
         if case .blurredBottomClearTop = direction {
             ciGradientFilter.point0.y = 0
             ciGradientFilter.point1.y = height - ciGradientFilter.point1.y
         }
-        guard let output = ciGradientFilter.outputImage,
-              let cgImage = CIContext().createCGImage(output, from: CGRect(x: 0, y: 0, width: width, height: height)) else {
-            return makeFallbackGradientImage(width: width, height: height)
-        }
-        return cgImage
-    }
-
-    private func makeFallbackGradientImage(width: CGFloat = 100, height: CGFloat = 100) -> CGImage {
-        let filter = CIFilter.linearGradient()
-        filter.color0 = CIColor.black
-        filter.color1 = CIColor.clear
-        filter.point0 = CGPoint(x: 0, y: height)
-        filter.point1 = CGPoint(x: 0, y: 0)
-        guard let output = filter.outputImage,
-              let cgImage = CIContext().createCGImage(output, from: CGRect(x: 0, y: 0, width: width, height: height)) else {
-            fatalError("VariableBlur: failed to create gradient image")
-        }
-        return cgImage
+        return CIContext().createCGImage(ciGradientFilter.outputImage!, from: CGRect(x: 0, y: 0, width: width, height: height))!
     }
 }
